e53ee517b2ad682b76b3acda1bc4324e7984dd3f
silversoft77 <silversoft77@gmail.com>
2023-08-14 23:32:51 -0400

    - added shopping page to the customer portal.

--- src/components/Header.js
+++ src/components/Header.js
@@ -49,10 +49,10 @@ export default function Header() {
             <ul className="flex flex-grow justify-end flex-wrap items-center">
               <li>
                 <a
-                  href="/"
+                  href="/customer/shop/"
                   className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out"
                 >
-                  Careers
+                  Shop List
                 </a>
               </li>
               <li>
@@ -68,17 +68,11 @@ export default function Header() {
                   className="inline-flex items-center px-4 py-2 mt-2 font-medium text-white transition duration-500 ease-in-out transform bg-transparent rounded-lg text-md md:mt-0 md:ml-4 bg-gray-900"
                   href="/"
                 >
-                  <span className="justify-center">Download</span>
-                  <svg
-                    className="w-3 h-3 fill-current text-gray-400 flex ml-2 -mr-1"
-                    viewBox="0 0 12 12"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
+                  <span className="justify-center">Admin</span>
                     <path
                       d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
                       fillRule="nonzero"
                     />
-                  </svg>
                 </a>
               </li>
             </ul>
---
+++ src/utils/const.js
@@ -0,0 +1,76 @@
+export const categories = [
+    {
+        cat_id: 1,
+        cat_name: "AI",
+        other: ""
+    },
+    {
+        cat_id: 2,
+        cat_name: "Ecommerce",
+        other: ""
+    },
+    {
+        cat_id: 3,
+        cat_name: "Blog",
+        other: ""
+    },
+];
+
+export const products = [
+    {
+        prod_id: 1,
+        prod_name: "AI",
+        price: 2000,
+        description: "This is what I wanted to say.",
+        image: "prod_01",
+        cat_id: 1
+    },
+    {
+        prod_id: 1,
+        prod_name: "AI",
+        price: 2000,
+        description: "This is what I wanted to say.",
+        image: "prod_01",
+        cat_id: 1
+    },
+    {
+        prod_id: 1,
+        prod_name: "AI",
+        price: 2000,
+        description: "This is what I wanted to say.",
+        image: "prod_01",
+        cat_id: 1
+    },
+    {
+        prod_id: 1,
+        prod_name: "AI",
+        price: 2000,
+        description: "This is what I wanted to say.",
+        image: "prod_01",
+        cat_id: 1
+    },
+    {
+        prod_id: 1,
+        prod_name: "AI",
+        price: 2000,
+        description: "This is what I wanted to say.",
+        image: "prod_01",
+        cat_id: 1
+    },
+    {
+        prod_id: 1,
+        prod_name: "AI",
+        price: 2000,
+        description: "This is what I wanted to say.",
+        image: "prod_01",
+        cat_id: 1
+    },
+    {
+        prod_id: 1,
+        prod_name: "AI",
+        price: 2000,
+        description: "This is what I wanted to say.",
+        image: "prod_01",
+        cat_id: 1
+    },
+];
