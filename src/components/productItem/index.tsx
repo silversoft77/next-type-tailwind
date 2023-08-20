1645a661352fccfff208a57637c3375a2ded1b8f
silversoft77 <silversoft77@gmail.com>
2023-08-20 14:29:57 -0400

    - added product, filtered product  reducers and contexts.

---
+++ src/components/productItem/index.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+export default function ProductItem({item}) {
+  return (
+    <div className="pb-2 mb-8 bg-gray-100 rounded-lg border border-solid border-gray-200 cursor-pointer">
+      <img
+        className="object-cover hover:opacity-80"
+        src={item.image}
+        alt=""
+        style={{ width: "300px", height: "200px" }}
+      />
+      <div className="pt-4 pl-3">
+        <div className="text-lg font-bold text-gray-600 pl-1">{item.name}</div>
+        <h3 className="text-1xl font-bold text-blue-500 pl-1">{`$ ${item.price}`}</h3>
+      </div>
+    </div>
+  );
+}
--- src/context/product_context.tsx
+++ src/context/product_context.tsx
@@ -2,93 +2,40 @@ import axios from "axios";
 import React, { useContext, useEffect, useReducer } from "react";
 import reducer from "../reducers/products_reducer";
 import { products_url as url } from "../utils/constants";
-import {
-  SIDEBAR_OPEN,
-  SIDEBAR_CLOSE,
-  GET_PRODUCTS_BEGIN,
-  GET_PRODUCTS_SUCCESS,
-  GET_PRODUCTS_ERROR,
-  GET_SINGLE_PRODUCT_BEGIN,
-  GET_SINGLE_PRODUCT_SUCCESS,
-  GET_SINGLE_PRODUCT_ERROR,
-} from "../actions";
+import { IAllProducts } from "../types/interface";
 
 const initialState = {
-  isSidebarOpen: false,
-  products_loading: false,
-  prodcuts_error: false,
   products: [],
-  featured_products: [],
-  single_product_loading: false,
-  error: false,
-  single_product: {},
 };
 
-type Product = {
-  isSidebarOpen: boolean;
-  products_loading: boolean;
-  prodcuts_error: boolean;
-  products: [];
-  featured_products: [];
-  single_product_loading: boolean;
-  error: boolean;
-  single_product: {};
-};
-
-interface ProductsContextType {
-  products: Product[]; 
-}
-
-const ProductsContext = React.createContext<ProductsContextType>({
-    products: []
+const ProductsContext = React.createContext<IAllProducts>({
+  ...initialState,
 });
 
 export const ProductsProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const openSidebar = () => {
-    dispatch({ type: SIDEBAR_OPEN });
-  };
-  const closeSidebar = () => {
-    dispatch({ type: SIDEBAR_CLOSE });
-  };
-
-  const fetchProducts = async (url) => {
-    dispatch({ type: GET_PRODUCTS_BEGIN });
-    try {
-      const response = await axios.get(url);
-      const products = response.data;
-
-      dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products });
-    } catch (error) {
-      dispatch({ type: GET_PRODUCTS_ERROR });
-    }
-  };
-
-  const fetchSingleProduct = async (url) => {
-    dispatch({ type: GET_SINGLE_PRODUCT_BEGIN });
-    try {
-      const response = await axios.get(url);
-      const singleProduct = response.data;
-      dispatch({ type: GET_SINGLE_PRODUCT_SUCCESS, payload: singleProduct });
-    } catch (error) {
-      dispatch({ type: GET_SINGLE_PRODUCT_ERROR });
-    }
-  };
-
   useEffect(() => {
     fetchProducts(url);
   }, []);
 
+  const fetchProducts = async (url: string) => {
+    try {
+      const response = await axios.get(url);
+      const products = response.data;
+      dispatch({ type: "GET_PRODUCTS_SUCCESS", payload: products });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <ProductsContext.Provider
-      value={{ ...state, openSidebar, closeSidebar, fetchSingleProduct }}
-    >
+    <ProductsContext.Provider value={{ ...state }}>
       {children}
     </ProductsContext.Provider>
   );
 };
-// make sure use
+
 export const useProductsContext = () => {
   return useContext(ProductsContext);
 };
